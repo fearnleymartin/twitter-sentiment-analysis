7eb8b8f549fe926e95581c6119cdd2539214bb1f
LucasFuentesValenzuela <lucasfuentes100@hotmail.com>
2016-12-06 12:36:34 +0100

    slight modifs + kaggle submission file

--- feature_repr.py
+++ feature_repr.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.cross_validation import cross_val_score
-
+from helpers_py import export_predictions
 
 def load_embeddings():
     return np.load('embeddings.npy')
@@ -59,6 +59,26 @@ def feature_representation(embeddings, positive_tweets, negative_tweets, vocab_d
         negative_tweets_feature_repr[index] = feature_repr
     return positive_tweets_feature_repr, negative_tweets_feature_repr
 
+def feature_representation_v2(embeddings, tweets, vocab_dict):
+    """
+    build feature representations of tweets by summing feature representations of the words in the tweets
+    :param embeddings:
+    :param positive_tweets:
+    :param negative_tweets:
+    :param vocab_dict:
+    :return:
+    """
+    tweets_feature_repr = np.zeros((len(tweets), 20))
+    for index, tweet in enumerate(tweets):
+        words = tweet.split(' ')
+        feature_repr = np.zeros(20)
+        for word in words:
+            if word in vocab_dict.keys():
+                feature_repr += embeddings[vocab_dict[word]]
+        tweets_feature_repr[index] = feature_repr
+
+    return tweets_feature_repr
+
 def concatenate_features(feature_1,feature_2):
     return np.concatenate((feature_1,feature_2),axis=1)
 
@@ -71,7 +91,7 @@ def regression(positive_tweets_feature_repr, negative_tweets_feature_repr):
     :return:
     """
     X = np.vstack((positive_tweets_feature_repr, negative_tweets_feature_repr))
-    Y = np.hstack((np.ones(positive_tweets_feature_repr.shape[0]), np.zeros(negative_tweets_feature_repr.shape[0])))
+    Y = np.hstack((np.ones(positive_tweets_feature_repr.shape[0]), -1*np.ones(negative_tweets_feature_repr.shape[0])))
     clf = LogisticRegression()
     clf.fit(X, Y)
     return clf, X, Y
@@ -84,17 +104,24 @@ if __name__ == '__main__':
 
     embeddings = load_embeddings()
     print("Embeddings loaded")
-    positive_tweets, negative_tweets = load_tweets()
+    positive_tweets, negative_tweets,test_tweets = load_tweets()
     vocab_dict = load_vocab()
-    positive_tweets_feature_repr, negative_tweets_feature_repr = feature_representation(embeddings, positive_tweets, negative_tweets, vocab_dict)
+    positive_tweets_feature_repr = feature_representation_v2(embeddings, positive_tweets, vocab_dict)
+    negative_tweets_feature_repr = feature_representation_v2(embeddings,negative_tweets,vocab_dict)
+    test_tweets_feature_repr = feature_representation_v2(embeddings,test_tweets,vocab_dict)
     print("First feature representation achieved")
     pos_tweets_lexicon_features,neg_tweets_lexicon_features,test_tweets_lexicon_features=load_lexicon_features()
     print("Lexicon features loaded")
     pos_tweets_features = concatenate_features(positive_tweets_feature_repr,pos_tweets_lexicon_features)
     neg_tweets_features = concatenate_features(negative_tweets_feature_repr,neg_tweets_lexicon_features)
+    test_tweets_features = concatenate_features(test_tweets_feature_repr,test_tweets_lexicon_features)
     print("Features concatenated")
-    print(pos_tweets_features.shape)
     clf, X, Y = regression(pos_tweets_features, neg_tweets_features)
     score = cross_validation(clf, X, Y)
     print(score)
+    predicted_labels=clf.predict(test_tweets_features)
+    print(predicted_labels)
+    export_predictions(predicted_labels)
+
+
 
