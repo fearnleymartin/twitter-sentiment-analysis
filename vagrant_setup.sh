3f4bb99d83a82f866911996271537147c43b22a9
Fearnley Martin <fearnleymartin@gmail.com>
2016-12-17 12:46:09 +0100

    vagrant setup correction

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt-get install git
-sudo apt-get install python-numpy python-scipy
+yes | sudo apt-get install git
+yes | sudo apt-get install python-numpy python-scipy
 pip install cython
 git clone https://github.com/facebookresearch/fastText.git
 cd fastText
