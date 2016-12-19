cfdb399eda6cdaa0cffc2671ee545ed92b7d8a7b
LucasFuentesValenzuela <lucasfuentes100@hotmail.com>
2016-12-19 15:23:48 +0100

    cleaning & spellcheck for fasttext

--- tweet_cleaning_fasttext.py
+++ tweet_cleaning_fasttext.py
@@ -7,28 +7,45 @@ from sklearn.linear_model import LogisticRegression
 #from Files_Clement.helpers import trainScore, simulateTest
 
 
+full = True;
+#Blank data
 train_pos = '../Tweets/train_pos.txt'
 train_neg = '../Tweets/train_neg.txt'
-train_pos_processed = 'train_pos_processed.txt'
-train_neg_processed = 'train_neg_processed.txt'
 test = '../Tweets/test_data.txt'
-test_processed = 'test_data_processed.txt'
 
-pos = open (train_pos).readlines ()
-neg = open (train_neg).readlines ()
-test = open (test).readlines ()
+if full:
+    train_pos = re.sub('.txt', '_full.txt', train_pos)
+    train_neg = re.sub('.txt', '_full.txt', train_neg)
+
+
+with open(train_pos, encoding='utf8') as f:
+    pos = f.readlines()
+with open(train_neg, encoding='utf8') as f:
+    neg = f.readlines()
+with open(test, encoding='utf8') as f:
+    test = f.readlines()
+
 
 
 pos, pos_cleaned = processTweets (pos, ['lower'], 'train_pos_clean')
 pos_correct = processTweets (pos_cleaned, ['spell check'], 'train_pos_correct') [1]
 neg, neg_cleaned = processTweets (neg, ['lower'], 'train_neg_clean')
 neg_correct = processTweets (neg_cleaned, ['spell check'], 'train_neg_correct') [1]
+test, test_cleaned = processTweets (test, ['lower'], 'test_clean')
+test_correct = processTweets (test_cleaned, ['spell check'], 'test_correct') [1]
 
 
-#with open ('train_full_correct.txt', 'w', encoding = 'utf-8') as f:
-#   f.write ('\n'.join (pos_correct) + '\n'.join (neg_correct))
 with open('train_pos_correct.txt','w', encoding='utf8') as f:
-    f.write('\n'.join (pos_correct))
+    for tweet in pos_correct:
+        tweet = re.sub("\n", '', tweet)
+        f.write(tweet + '\n')
 
 with open('train_neg_correct.txt','w', encoding='utf8') as f:
-    f.write('\n'.join(neg_correct))
+    for tweet in neg_correct:
+        tweet = re.sub("\n", '', tweet)
+        f.write(tweet + '\n')
+
+with open('test_correct.txt','w', encoding='utf8') as f:
+    for tweet in test_correct:
+        tweet = re.sub("\n", '', tweet)
+        f.write(tweet + '\n')
