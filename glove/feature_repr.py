0373973a4a9fc869858e6787f0e66da9d08971ae
Fearnley Martin <fearnleymartin@gmail.com>
2016-12-19 18:15:47 +0100

    glove tidying, commenting, refactorring

--- glove/feature_repr.py
+++ glove/feature_repr.py
@@ -1,77 +1,56 @@
 import numpy as np
-from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.cross_validation import cross_val_score
 from helpers_py import export_predictions
 from sklearn.ensemble import RandomForestClassifier
 
 
-def load_embeddings():
-    return np.load('embeddings_cleaned.npy')
+# Input file paths
+train_pos = '../data/processed/train_pos_processed_full.txt'
+train_neg = '../data/processed/train_neg_processed_full.txt'
+test = '../data/processed/test_data_processed.txt'
+embeddings = 'embeddings.npy'
+vocabulary = 'vocab_cut.txt'
+
+# Output file paths
+predictions_output = 'results/predictions.txt'
 
 
-def load_tweets(full=False, cleaned=True):
-    if not full:
-        if cleaned:
-            with open('train_pos_cleaned.txt', encoding='utf8') as file:
-                pos = file.readlines()
-            with open('train_neg_cleaned.txt', encoding='utf8') as file:
-                neg = file.readlines()
-        else:
-            with open('train_pos.txt', encoding='utf8') as file:
-                pos = file.readlines()
-            with open('train_neg.txt', encoding='utf8') as file:
-                neg = file.readlines()
-    else:
-        if cleaned:
-            with open('train_pos_full_cleaned.txt', encoding='utf8') as file:
-                pos = file.readlines()
-            with open('train_neg_full_cleaned.txt', encoding='utf8') as file:
-                neg = file.readlines()
-        else:
-            with open('train_pos_full.txt', encoding='utf8') as file:
-                pos = file.readlines()
-            with open('train_neg_full.txt', encoding='utf8') as file:
-                neg = file.readlines()
-    with open('test_data.txt') as file:
+def load_tweets(train_pos, train_neg, test):
+    """
+    Loads each set of tweets from file to list
+    :return: list of pos tweets, list of negative tweets, list of test tweets
+    """
+    with open(train_pos, encoding='utf8') as file:
+        pos = file.readlines()
+    with open(train_neg, encoding='utf8') as file:
+        neg = file.readlines()
+    with open(test) as file:
         test = file.readlines()
-    return pos, neg, test
-
-
-def load_lexicon_features(full=False):
-    if not full:
-        pos = np.load('pos_tweets_lexicon_features.npy')
-        neg = np.load('neg_tweets_lexicon_features.npy')
-    else:
-        pos = np.load('pos_tweets_full_lexicon_features.npy')
-        neg = np.load('neg_tweets_full_lexicon_features.npy')
-
-    test = np.load('test_tweets_lexicon_features.npy')
-
     return pos, neg, test
 
 
 def load_vocab():
     """
-    build dictionary of vocabulary to recover index of word in embeddings
+    Build dictionary of vocabulary to recover index of word in embeddings
     ex: vocab_dict[the] = 3
+    :return: {word:index,...}
     """
     vocab_dict = {}
-    with open('vocab_cut_cleaned.txt') as file:
+    with open(vocabulary) as file:
         vocab = file.readlines()
     for index, word in enumerate(vocab):
         vocab_dict[word] = index
     return vocab_dict
 
 
-def feature_representation_v2(embeddings, tweets, vocab_dict):
+def feature_representation(embeddings, tweets, vocab_dict):
     """
     build feature representations of tweets by summing feature representations of the words in the tweets
-    :param embeddings:
-    :param tweets:
-    :param vocab_dict:
+    :param embeddings: dict {word_index, vector_representation_of_word}
+    :param tweets: list of tweets
+    :param vocab_dict: lookup dictionary for getting index from word
     :return:
     """
-
     tweets_feature_repr = np.zeros((len(tweets), 20))
     for index, tweet in enumerate(tweets):
         words = tweet.split(' ')  # split into words
@@ -80,60 +59,40 @@ def feature_representation_v2(embeddings, tweets, vocab_dict):
             if word in vocab_dict.keys():
                 feature_repr += embeddings[vocab_dict[word]]
         tweets_feature_repr[index] = feature_repr
-
     return tweets_feature_repr
-
-
-def concatenate_features(feature_1, feature_2):
-    return np.concatenate((feature_1, feature_2), axis=1)
 
 
 def regression(positive_tweets_feature_repr, negative_tweets_feature_repr):
     """
     Build the data matrix and labels array from positive and negative tweet feature representation
     Run logistic regression classifier
-    :param positive_tweets_feature_repr:
-    :param negative_tweets_feature_repr:
-    :return:
+    :param positive_tweets_feature_repr: list of positive tweets represented as vectors
+    :param negative_tweets_feature_repr: list of negative tweets represented as vectors
+    :return: classifier, data matrix, labels
     """
     X = np.vstack((positive_tweets_feature_repr, negative_tweets_feature_repr))
     Y = np.hstack((np.ones(positive_tweets_feature_repr.shape[0]), 0*np.ones(negative_tweets_feature_repr.shape[0])))
-    # clf = LogisticRegression()
-    # clf = RandomForestClassifier(n_estimators=500, max_features='log2', max_depth=8, min_samples_leaf=2)
     clf = RandomForestClassifier()
-
     clf.fit(X, Y)
     return clf, X, Y
 
 
-def cross_validation(clf, X, Y):
-    return cross_val_score(clf, X, Y, cv=5, scoring='accuracy')
-
-
 if __name__ == '__main__':
-    embeddings = load_embeddings()
-    print("Embeddings loaded")
-    positive_tweets, negative_tweets, test_tweets = load_tweets(full=False, cleaned=True)
+    embeddings = np.load(embeddings)
+    positive_tweets, negative_tweets, test_tweets = load_tweets(train_neg, train_neg, test)
     vocab_dict = load_vocab()
-    positive_tweets_feature_repr = feature_representation_v2(embeddings, positive_tweets, vocab_dict)
-    negative_tweets_feature_repr = feature_representation_v2(embeddings, negative_tweets, vocab_dict)
-    test_tweets_feature_repr = feature_representation_v2(embeddings, test_tweets, vocab_dict)
-    print("First feature representation achieved")
-
-    # pos_tweets_lexicon_features, neg_tweets_lexicon_features, test_tweets_lexicon_features = load_lexicon_features(full=True)
-    # print("Lexicon features loaded")
-    # pos_tweets_features = concatenate_features(positive_tweets_feature_repr, pos_tweets_lexicon_features)
-    # neg_tweets_features = concatenate_features(negative_tweets_feature_repr, neg_tweets_lexicon_features)
-    # test_tweets_features = concatenate_features(test_tweets_feature_repr, test_tweets_lexicon_features)
-    # print("Features concatenated")
+    positive_tweets_feature_repr = feature_representation(embeddings, positive_tweets, vocab_dict)
+    negative_tweets_feature_repr = feature_representation(embeddings, negative_tweets, vocab_dict)
+    test_tweets_feature_repr = feature_representation(embeddings, test_tweets, vocab_dict)
+    print("feature representation created")
 
     clf, X, Y = regression(positive_tweets_feature_repr, negative_tweets_feature_repr)
-    score = cross_validation(clf, X, Y)
+    score = cross_val_score(clf, X, Y, cv=5, scoring='accuracy')
     print(score)
 
-    # predicted_labels = clf.predict(test_tweets_lexicon_features)
-    # print(predicted_labels)
-    # export_predictions(predicted_labels,'submission_lexicon_only_features')
+    predicted_labels = clf.predict(test_tweets_feature_repr)
+    print(predicted_labels)
+    export_predictions(predicted_labels, predictions_output)
 
 
 
