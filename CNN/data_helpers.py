f533a52145464cc38b2d7f2269404c2435d40821
Fearnley Martin <fearnleymartin@gmail.com>
2016-12-19 15:42:56 +0100

    CNN data helper modifs removing tokenisation because already done on file with nltk twwet tokeniser option to partially load full file (to avoid memory error)

--- CNN/data_helpers.py
+++ CNN/data_helpers.py
@@ -9,31 +9,31 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    string = re.sub(r"\'s", " \'s", string)
-    string = re.sub(r"\'ve", " \'ve", string)
-    string = re.sub(r"n\'t", " n\'t", string)
-    string = re.sub(r"\'re", " \'re", string)
-    string = re.sub(r"\'d", " \'d", string)
-    string = re.sub(r"\'ll", " \'ll", string)
-    string = re.sub(r",", " , ", string)
-    string = re.sub(r"!", " ! ", string)
-    string = re.sub(r"\(", " \( ", string)
-    string = re.sub(r"\)", " \) ", string)
-    string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
-    return string.strip().lower()
+    # string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    # string = re.sub(r"\'s", " \'s", string)
+    # string = re.sub(r"\'ve", " \'ve", string)
+    # string = re.sub(r"n\'t", " n\'t", string)
+    # string = re.sub(r"\'re", " \'re", string)
+    # string = re.sub(r"\'d", " \'d", string)
+    # string = re.sub(r"\'ll", " \'ll", string)
+    # string = re.sub(r",", " , ", string)
+    # string = re.sub(r"!", " ! ", string)
+    # string = re.sub(r"\(", " \( ", string)
+    # string = re.sub(r"\)", " \) ", string)
+    # string = re.sub(r"\?", " \? ", string)
+    # string = re.sub(r"\s{2,}", " ", string)
+    # return string.strip().lower()
+    return string.strip()
 
-
-def load_data_and_labels(positive_data_file, negative_data_file):
+def load_data_and_labels(positive_data_file, negative_data_file, datasize=1000000):
     """
     Loads MR polarity data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
     """
     # Load data from files
-    positive_examples = list(open(positive_data_file, "r",encoding="utf8").readlines())
+    positive_examples = list(open(positive_data_file, "r",encoding="utf8").readlines())[0:datasize]
     positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open(negative_data_file, "r",encoding="utf8").readlines())
+    negative_examples = list(open(negative_data_file, "r",encoding="utf8").readlines())[0:datasize]
     negative_examples = [s.strip() for s in negative_examples]
     # Split by words
     x_text = positive_examples + negative_examples
