d5ad0178c47d4688c4d2f2b7d3e52dc36c3b93b5
Fearnley Martin <fearnleymartin@gmail.com>
2016-12-20 13:39:38 +0100

    glove bug fix

--- glove/classifier.py
+++ glove/classifier.py
@@ -2,11 +2,11 @@ import numpy as np
 from sklearn.cross_validation import cross_val_score
 from helpers_py import export_predictions
 from sklearn.ensemble import RandomForestClassifier
-
+import re
 
 # Input file paths
-train_pos = '../data/processed/train_pos_processed_full.txt'
-train_neg = '../data/processed/train_neg_processed_full.txt'
+train_pos = '../data/processed/train_pos_processed.txt'
+train_neg = '../data/processed/train_neg_processed.txt'
 test = '../data/processed/test_data_processed.txt'
 embeddings = 'embeddings.npy'
 vocabulary = 'vocab_cut.txt'
@@ -29,7 +29,7 @@ def load_tweets(train_pos, train_neg, test):
     return pos, neg, test
 
 
-def load_vocab():
+def load_vocab(vocabulary):
     """
     Build dictionary of vocabulary to recover index of word in embeddings
     ex: vocab_dict[the] = 3
@@ -39,6 +39,7 @@ def load_vocab():
     with open(vocabulary) as file:
         vocab = file.readlines()
     for index, word in enumerate(vocab):
+        word = re.sub('\n', '', word)
         vocab_dict[word] = index
     return vocab_dict
 
@@ -58,6 +59,7 @@ def feature_representation(embeddings, tweets, vocab_dict):
         for word in words:
             if word in vocab_dict.keys():
                 feature_repr += embeddings[vocab_dict[word]]
+
         tweets_feature_repr[index] = feature_repr
     return tweets_feature_repr
 
@@ -80,14 +82,15 @@ def regression(positive_tweets_feature_repr, negative_tweets_feature_repr):
 if __name__ == '__main__':
     embeddings = np.load(embeddings)
     positive_tweets, negative_tweets, test_tweets = load_tweets(train_neg, train_neg, test)
-    vocab_dict = load_vocab()
+    vocab_dict = load_vocab(vocabulary)
+    print(len(vocab_dict))
     positive_tweets_feature_repr = feature_representation(embeddings, positive_tweets, vocab_dict)
     negative_tweets_feature_repr = feature_representation(embeddings, negative_tweets, vocab_dict)
     test_tweets_feature_repr = feature_representation(embeddings, test_tweets, vocab_dict)
     print("feature representation created")
 
     clf, X, Y = regression(positive_tweets_feature_repr, negative_tweets_feature_repr)
-    score = cross_val_score(clf, X, Y, cv=5, scoring='accuracy')
+    score = cross_val_score(clf, X, Y, cv=4, scoring='accuracy')
     print(score)
 
     predicted_labels = clf.predict(test_tweets_feature_repr)
